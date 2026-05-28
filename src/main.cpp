670a72734be09105375d61f724777c7f51901f43
joshjyu <jae@joshyu.com>
2026-05-27 22:21:18 -0700

    Add exit confirmation overlay menu
    Continues the translation to C++.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <ncurses.h>
+#include <string>
 #include <utility>
+#include <vector>
 
 /// @brief Calculates Y and X coordinates to center text on the screen.
 ///
@@ -17,6 +19,56 @@ std::pair<int, int> getCenteredCoords(int textLen, int offsetY = 0) {
     int y = (maxY / 2) + offsetY;
 
     return {y, x};
+}
+
+/// @brief Prompts the user to confirm exiting.
+///
+/// @return True if the user confirms exit. False otherwise.
+bool runConfirmExit() {
+    std::vector<std::string> options = {"YES", "NO"};
+
+    // Default to NO
+    int currentSelection = 1;
+
+    while (true) {
+        clear();
+
+        std::string warnTitle = "Exit Game?";
+        std::string warnDesc = "Are you sure you want to quit?";
+
+        // Print the title and description
+        auto [titleY, titleX] = getCenteredCoords(warnTitle.size(), -2);
+        auto [descY, descX] = getCenteredCoords(warnDesc.size(), -1);
+        mvaddstr(titleY, titleX, warnTitle.c_str());
+        mvaddstr(descY, descX, warnDesc.c_str());
+
+        // Add dynamic ">" marker next to selections
+        for (int i = 0; i < (int)options.size(); i++) {
+            std::string label =
+                (i == currentSelection) ? "> " + options[i] : "  " + options[i];
+            auto [y, x] = getCenteredCoords(label.size(), 1 + i);
+
+            // Highlight the selection by swapping the FG and BG
+            if (i == currentSelection) {
+                attron(A_REVERSE);
+                mvaddstr(y, x, label.c_str());
+                attroff(A_REVERSE);
+            } else {
+                mvaddstr(y, x, label.c_str());
+            }
+        }
+
+        refresh();
+        int key = getch();
+
+        if (key == KEY_UP || key == 'w' || key == 'W')
+            currentSelection = std::max(0, currentSelection - 1);
+        else if (key == KEY_DOWN || key == 's' || key == 'S')
+            currentSelection =
+                std::min((int)options.size() - 1, currentSelection + 1);
+        else if (key == KEY_ENTER || key == 10 || key == 13)
+            return currentSelection == 0;
+    }
 }
 
 /// @brief Initializes color pairs and sets the window background.
